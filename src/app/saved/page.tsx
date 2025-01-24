1b51b445f6d1294e6330c2ecb85a1716bb54b3a8
Angelo Gaona <ang.dev@hotmail.com>
2025-01-24 08:55:16 -0500

    View Saved in progress

---
+++ src/app/saved/page.tsx
@@ -0,0 +1,16 @@
+const Saved = () => {
+    return (
+        <section className="w-full h-[65.39vh] flex relative">
+            <div className="bg-[#220f3d] z-10 flex justify-center items-center flex-col flex-wrap w-full max-w-[1300px] h-[450px] m-auto rounded-lg px-4 sm:px-8">
+                <h1 className="text-6xl sm:text-5xl md:text-4xl lg:text-6xl font-bold bg-gradient-to-r from-[#6800ff] to-[#ff00ff] text-transparent bg-clip-text">
+                    Area en construcción
+                </h1>
+                <p className="text-xl sm:text-lg text-white mt-4">
+                    pronto podran guardar pelicuas
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Saved;
